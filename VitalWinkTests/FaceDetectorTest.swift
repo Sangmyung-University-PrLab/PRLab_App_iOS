9d146cd79ee52bfc51a53964483b0f8c0238d9dd
YouHojoon <dbghwns66@daum.net>
2023-05-30 13:53:17 +0900

    fix: OpenCVWrapper, FaceDetector - 피부 분할 후 픽셀들의 평균을 낸 BGR 값을 반환하는 것으로 변경

--- VitalWinkTests/FaceDetectorTest.swift
+++ VitalWinkTests/FaceDetectorTest.swift
@@ -8,48 +8,27 @@
 import XCTest
 import Combine
 import UIKit
-//import Dependencies
+import Dependencies
 
 @testable import VitalWink
 final class FaceDetectorTest: XCTestCase {
-//    let camera = try! Camera()
-//    @Dependency(\.faceDetector) var sut
+    
+    @Dependency(\.faceDetector) var sut: FaceDetector
     var subscriptions = Set<AnyCancellable>()
     
-//    func test_whenDetect_withNoFace(){
-//        camera.start()
-//        camera.sink(receiveValue: {[weak self] buffer in
-//            guard let strongSelf = self else {
-//                return
-//            }
-//            sut.detect(UIImage(sampleBuffer: buffer)).sink(receiveCompletion: {
-//                switch $0{
-//                case .finished:
-//                    _ = 1
-//                case .failure(let error):
-//                    let detectorError = error as? FaceDetector.FaceDetectorError
-//                    XCTAssertNotNil(detectorError)
-//                    XCTAssertTrue(detectorError == FaceDetector.FaceDetectorError.getEmptyUIImage)
-//                }
-//            }, receiveValue: {
-//                XCTAssertTrue($0 == CGRect.zero)
-//            }).store(in: &strongSelf.subscriptions)
-//        }).store(in: &subscriptions)
-//    }
-//                                  
-//    func test_whenDetect_withEmptyUIImage(){
-//        sut.detect(UIImage()).sink(receiveCompletion: {
-//            switch $0{
-//            case .finished:
-//                _ = 1
-//            case .failure(let error):
-//                let detectorError = error as? FaceDetector.FaceDetectorError
-//                XCTAssertNotNil(detectorError)
-//                XCTAssertTrue(detectorError == FaceDetector.FaceDetectorError.getEmptyUIImage)
-//            }
-//        }, receiveValue: {
-//            XCTAssertTrue($0 == CGRect.zero)
-//        }).store(in: &subscriptions)
-//         
-//    }
+    func test_whenDetect_withEmptyUIImage(){
+        sut.detect(UIImage()).sink(receiveCompletion: {
+            switch $0{
+            case .finished:
+                _ = 1
+            case .failure(let error):
+                let detectorError = error as? FaceDetector.FaceDetectorError
+                XCTAssertNotNil(detectorError)
+                XCTAssertTrue(detectorError == FaceDetector.FaceDetectorError.getEmptyUIImage)
+            }
+        }, receiveValue: {
+            XCTAssertTrue($0 == CGRect.zero)
+        }).store(in: &subscriptions)
+         
+    }
 }
