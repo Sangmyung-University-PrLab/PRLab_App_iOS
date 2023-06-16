61dc5520624af34de8e8086fe9efa98731e9260d
YouHojoon <dbghwns66@daum.net>
2023-06-16 17:10:59 +0900

    fix:SnsUserInfoService - getSnsUserInfo 임시로 모두 카카오 유저 조회로 처리

--- VitalWink/User/SnsUserInfoService.swift
+++ VitalWink/User/SnsUserInfoService.swift
@@ -37,7 +37,27 @@ final class SnsUserInfoService{
             }
            
         default:
-            break
+            return await withCheckedContinuation{continuation in
+                UserApi.shared.me{user, error in
+                    guard error == nil else{
+                        continuation.resume(returning: .failure(error!))
+                        return
+                    }
+                    
+                    let email = user?.kakaoAccount?.email ?? ""
+                    let gender: UserModel.Gender = user?.kakaoAccount?.gender == .Male ? .man : .woman
+                    let birthdayString = user?.kakaoAccount?.birthday ?? ""
+                    let birthYearString = user?.kakaoAccount?.birthyear ?? ""
+                    let dateString = birthYearString + birthdayString
+                    
+                    let dateformatter = DateFormatter()
+                    dateformatter.dateFormat = "yyyyMMdd"
+                    
+                    let birthday = dateString.isEmpty ? .now : dateformatter.date(from: dateString)!
+                    
+                    continuation.resume(returning: .success(.init(id: "", password: "", email: email, gender: gender, birthday: birthday, type: type)))
+                }
+            }
         }
     }
     
