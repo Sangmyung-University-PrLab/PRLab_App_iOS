8f6a733043935bdbf18ae65ada197b0e7570dd87
YouHojoon <dbghwns66@daum.net>
2023-07-05 16:30:36 +0900

    MetricChartView, MerticChart - 차트 아이템 보이는 것으로 높낮이 조절 기능 추가 - 아이템 값 새로고침 기능 추가

--- VitalWink/Monitoring/MetricChart.swift
+++ VitalWink/Monitoring/MetricChart.swift
@@ -12,12 +12,28 @@ struct MetricChart: ReducerProtocol{
     init(){
         dateFormatter.dateFormat = "yyyy/MM/dd"
     }
+    struct ChartData: Equatable{
+        let value: MinMaxType<Float>
+        var isVisible:Bool
+    }
     struct State: Equatable{
+//        static func == (lhs: MetricChart.State, rhs: MetricChart.State) -> Bool {
+//            return lhs.period == rhs.period && lhs.basisDate == rhs.basisDate && lhs.selected == rhs.selected
+//            && lhs.baseRange == rhs.baseRange && lhs.datas.elementsEqual(rhs.datas){
+//                return $0.value.isVisable == $1.value.isVisable && $1.value.value == $0.value.value
+//            }
+//        }
+        
+        var sortedKeys: [String]{
+            return datas.keys.sorted()
+        }
+        
         @BindingState var period: Period = .week
         fileprivate var basisDate: Date? = nil
-        fileprivate(set) var datas: [String : MinMaxType<Float>?] = [:]
+        fileprivate(set) var datas: [String : ChartData?] = [:]
         fileprivate(set) var selected: Int? = nil
-        fileprivate(set) var baseRange: MinMaxType<Float> = .init(min: 0, max: 0)
+        fileprivate(set) var baseRange: MinMaxType<Float>? = nil
+    
     }
     
     enum Action: BindableAction{
@@ -27,17 +43,63 @@ struct MetricChart: ReducerProtocol{
         case selectItem(_ index: Int?)
         case errorHandling(Error)
         case refresh(Metric)
+        case changeVisible(String,Bool)
+        case setBaseRange
     }
-    
+    enum CancelId: Hashable{
+        case setBaseRange
+    }
     var body: some ReducerProtocol<State, Action> {
         BindingReducer()
         Reduce{state, action in
             switch action{
+            case .changeVisible(let key,let isVisible):
+                
+                guard var data = state.datas[key, default: nil] else{
+                    return .none
+                }
+                data.isVisible = isVisible
+                state.datas[key] = data
+                
+                return
+                    .cancel(id:CancelId.setBaseRange)
+                    .merge(with: .run{_ in
+                        try await Task.sleep(nanoseconds: UInt64(1_000_000_000 * 0.5))
+                    })
+                .concatenate(with: .send(.setBaseRange, animation:.linear).cancellable(id: CancelId.setBaseRange, cancelInFlight: true))
+                
+            case .setBaseRange:
+                let visibleDatas = state.datas
+                    .compactMapValues{$0}
+                    .filter{$0.value.isVisible}
+                    .map{$0.value.value}
+                
+                
+                guard !visibleDatas.isEmpty else{
+                    state.baseRange = nil
+                    return .none
+                }
+                
+                
+                state.baseRange = visibleDatas.reduce(MinMaxType(min: visibleDatas.first?.min ?? 0, max: visibleDatas.first?.max ?? 0)){
+                    let min = $0.min < $1.min ? $0.min : $1.min
+                    let max = $0.max > $1.max ? $0.max : $1.max
+                    
+                    return MinMaxType(min: min, max: max)
+                }
+                
+                
+                
+                
+                if state.baseRange!.max == 0 && state.baseRange!.min == 0{
+                    state.baseRange = nil
+                }
+                return .none
             case .binding(\.$period):
                 state.datas = [:]
                 state.basisDate = nil
                 state.selected = nil
-                
+                state.baseRange = nil
                 if state.period == .day{
                     dateFormatter.dateFormat = "MM:dd"
                 }
@@ -65,7 +127,7 @@ struct MetricChart: ReducerProtocol{
                     state.basisDate = date
                 }
                 
-                let dateArray = date.dateArrayInPeriod(end: Date(timeInterval: -60 * 60 * 24 * 7 * 2, since: date))
+                let dateArray = date.dateArrayInPeriod(end: Date(timeInterval: -60 * 60 * 24 * 7 * 1, since: date))
                 dateArray.forEach{
                     state.datas.updateValue(nil, forKey: dateFormatter.string(from: $0))
                 }
@@ -81,15 +143,7 @@ struct MetricChart: ReducerProtocol{
        
             case .responseMetricDatas(let datas):
                 datas.forEach{
-                    state.datas[dateFormatter.string(from: $0.basisDate)] = $0.value
-                }
-//                print(state.datas)
-                state.baseRange = state.datas.compactMapValues{$0}.map{$0.value}
-                    .reduce(MinMaxType(min: datas.first?.value.min ?? 0, max: datas.first?.value.max ?? 0)){
-                    let min = $0.min < $1.min ? $0.min : $1.min
-                    let max = $0.max > $1.max ? $0.max : $1.max
-
-                    return MinMaxType(min: min, max: max)
+                    state.datas[dateFormatter.string(from: $0.basisDate)] = .init(value: $0.value,isVisible:false)
                 }
                 
                 return .none
--- VitalWink/Monitoring/View/MetricChartView.swift
+++ VitalWink/Monitoring/View/MetricChartView.swift
@@ -21,56 +21,67 @@ struct MetricChartView: View{
     
     var body: some View{
         WithViewStore(store, observe: {$0}){viewStore in
-            GeometryReader{outerProxy in
-                HStack{
-                    Path{
-                        $0.move(to: CGPoint(x: outerProxy.size.width - 30, y: 0))
-                        $0.addLine(to: CGPoint(x: outerProxy.size.width - 30, y: outerProxy.size.height - 12))
-                        $0.addLine(to: CGPoint(x: 0, y: outerProxy.size.height - 12))
-                    }.stroke(Color.gray)
-                    .frame(width: outerProxy.size.width - 30, height: outerProxy.size.height - 12)
-                        .overlay{
-                            GeometryReader{proxy in
-                                ScrollViewReader{scrollReader in
-                                    ScrollView(.horizontal,showsIndicators: false){
-                                        LazyHStack(spacing: 10){
-                                            ForEach(keys, id: \.self){key in
-                                                let yyyyMMdd = key.split(separator: "/").map{Int($0)!}
-                                                let x = yyyyMMdd[2] == 1 ? "\(yyyyMMdd[1])/\(yyyyMMdd[2])" : "\(yyyyMMdd[2])"
-                                                
-                                                MetricChartItemView(x:x,y: viewStore.datas[key, default:nil], baseRange: viewStore.baseRange, baseHeight: Float(outerProxy.size.height) - 12)
-                                                    .frame(width: (proxy.size.width - 10 * 6) / 7)
-                                                    .onAppear{
-                                                        guard let first = keys.first else{
-                                                            return
-                                                        }
-                                                        
-                                                        if first == key{
-                                                            viewStore.send(.refresh(metric))
-                                                        }
+                GeometryReader{proxy in
+                    HStack(spacing:10){
+                        ScrollViewReader{scrollReader in
+                            ScrollView(.horizontal,showsIndicators: false){
+                                LazyHStack(spacing: 10){
+                                    ForEach(viewStore.sortedKeys, id: \.self){key in
+                                        let yyyyMMdd = key.split(separator: "/").map{Int($0)!}
+                                        let x = yyyyMMdd[2] == 1 ? "\(yyyyMMdd[1])/\(yyyyMMdd[2])" : "\(yyyyMMdd[2])"
+                                        
+                                        MetricChartItemView(x:x,y: viewStore.datas[key, default: nil]?.value, baseRange: viewStore.baseRange, baseHeight: Float(proxy.size.height) - 30)
+                                            .frame(width: (proxy.size.width - 50 - 10 * 6) / 7)
+                                            .onAppear{
+                                                Task{
+                                                    viewStore.send(.changeVisible(key,true))
+                                                    guard let first = viewStore.sortedKeys.first else{
+                                                        return
                                                     }
-                                            }
-                                        }.scaleEffect(x: -1.0, y: 1.0, anchor: .center)
-                                      
-                                    }.scaleEffect(x: -1.0, y: 1.0, anchor: .center)
 
+                                                    if first == key{
+                                                        viewStore.send(.refresh(metric))
+                                                    }
+                                                }
+                                               
+                                            }.onDisappear{
+                                                Task{
+                                                    viewStore.send(.changeVisible(key,false))
+                                                }
+                                                
+                                            }
+                                    }
                                 }
+                            
+                                .scaleEffect(x: -1.0, y: 1.0, anchor: .center)
+                                
                             }
-                           
+                            .frame(width: proxy.size.width - 30)
+                            .scaleEffect(x: -1.0, y: 1.0, anchor: .center)
+                            
                         }
-                    
-                    VStack{
-                        Text(numberFormatter.string(for: viewStore.baseRange.max)!)
-                            .font(.notoSans(size: 12))
-                        Spacer()
-                        Text(numberFormatter.string(for: viewStore.baseRange.min)!)
-                            .font(.notoSans(size: 12))
-                    }.frame(height:outerProxy.size.height)
+                        .overlay{
+                            Path{
+                                $0.move(to: CGPoint(x:0, y:proxy.size.height - 30))
+                                $0.addLine(to: CGPoint(x:proxy.size.width - 30, y: proxy.size.height - 30))
+                                $0.addLine(to: CGPoint(x:proxy.size.width - 30, y: 0))
+                            }.stroke(Color.gray)
+                            
+                        }
+                        
+                        VStack(spacing: 0){
+                            if let baseRange = viewStore.baseRange{
+                                Text(numberFormatter.string(for: baseRange.max)!)
+                                Spacer()
+                                Text(numberFormatter.string(for: baseRange.min)!)
+                            }
+                            Spacer().frame(height:30)
+                        }.font(.notoSans(size: 12, weight: .medium))
+                        .frame(height:proxy.size.height + 12)
+                    }
+                 
                 }
-                .frame(height: outerProxy.size.height)
-                .onChange(of:viewStore.datas){
-                    keys = $0.keys.sorted()
-                }
+             
                 .onChange(of: viewStore.period){_ in
                     viewStore.send(.selectItem(nil))
                     viewStore.send(.fetchMetricDatas(metric, .now))
@@ -79,11 +90,11 @@ struct MetricChartView: View{
                     viewStore.send(.fetchMetricDatas(metric, .now))
                 }
             }
-        }
+        
         
         
     }
-    @State var keys:[String] = []
+
     private let metric: Metric
     private let store: StoreOf<MetricChart>
     private let numberFormatter: NumberFormatter
@@ -91,11 +102,16 @@ struct MetricChartView: View{
 }
 
 struct MetricChartItemView: View{
-    init(x:String, y: MinMaxType<Float>? = nil, baseRange: MinMaxType<Float>, baseHeight: Float) {
+    init(x:String, y: MinMaxType<Float>? = nil, baseRange: MinMaxType<Float>?, baseHeight: Float) {
         self.baseRange = baseRange
         self.x = x
         self.y = y
-        self.ratio = (baseRange.max - baseRange.min) / baseHeight
+        if let baseRange = baseRange{
+            self.ratio = (baseRange.max - baseRange.min) / baseHeight
+        }
+        else{
+            self.ratio = 1
+        }
         
         self.baseHeight = CGFloat(baseHeight)
     }
@@ -103,26 +119,31 @@ struct MetricChartItemView: View{
     var body: some View{
         VStack(spacing:0){
             if let value = y {
-                let upper = baseRange.max - value.max
-                
-                if upper != 0{
-                    Spacer()
-                        .frame(height: CGFloat(upper / ratio))
+                if let baseRange = self.baseRange{
+                    let upper = max(baseRange.max - value.max, 0)
                     
-                }
-                Capsule()
-                    .frame(maxWidth: 5, minHeight:3)
-                
-                let lower = value.min - baseRange.min
-                
-                if lower != 0 && value.min != value.max{
-                    Spacer()
-                        .frame(height:CGFloat(lower / ratio))
-                }
-                else if value.max == value.min{
-                   
-                    Spacer()
-                        .frame(height: max(baseHeight - CGFloat(upper / ratio) - 4, 0))
+                    if upper != 0{
+                        Spacer()
+                            .frame(height: CGFloat(upper / ratio))
+                        
+                    }
+                    Capsule()
+                        .frame(maxWidth: 5, minHeight:3)
+                    
+                    let lower = max(value.min - baseRange.min,0)
+                    
+                    if lower != 0 && value.min != value.max{
+                        Spacer()
+                            .frame(height:CGFloat(lower / ratio))
+                    }
+                    else if value.max == value.min && value != baseRange{
+                        
+                        Spacer()
+                            .frame(height: max(baseHeight - CGFloat(upper / ratio) - 4, 0))
+                    }
+                }else{
+                    Capsule()
+                        .frame(maxWidth: 5, minHeight:3)
                 }
             }
             else{
@@ -131,14 +152,16 @@ struct MetricChartItemView: View{
             }
             Text(x)
                 .font(.notoSans(size: 12))
-                .foregroundColor(.gray).padding(.top, 3)
+                .foregroundColor(.gray)
+                .padding(.top, 10)
+                .frame(height:30)
         }
     }
 
     private let ratio: Float
     private let x: String
     private let y: MinMaxType<Float>?
-    private let baseRange: MinMaxType<Float>
+    private let baseRange: MinMaxType<Float>?
     private let baseHeight: CGFloat
 }
 
